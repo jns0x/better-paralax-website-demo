4967c31e78a16c684e2bc577436e9b656e9ce5a4
Pawel Janus <yanus@macbook-pro-pawel.home>
2018-05-20 21:26:11 +0200

    part-7.0

--- js/functions.js
+++ js/functions.js
@@ -22,4 +22,22 @@ $(window).scroll(function(){
 		});
 	}
 
+	if (wScroll > $('.large-window').offset().top - $(window).height()){
+		$('.large-window').css({ 'background-position': 'center ' + (wScroll - $('.large-window').offset().top) +'px'});
+		const opacity = (wScroll - $('.large-window').offset().top+400)/ (wScroll/5) 
+		$('.window-tint').css({'opacity': opacity})
+
+	}
+
+	if (wScroll > $('.large-window').offset().top - $(window).height()) {
+		const offset = Math.min(0, wScroll - $('.blog-posts').offset().top + $(window).height()-350);
+		$('.post-1').css({ 'transform': 'translate(' + offset + 'px, ' + Math.abs(offset * 0.2) + 'px)' });
+
+		$('.post-3').css({ 'transform': 'translate(' + Math.abs(offset) + 'px, ' + Math.abs(offset* 0.2) + 'px)' });
+
+	
+	}
+
+
+
 });
